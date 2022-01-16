c03d25b109140402d98424eeeadf9eca24d0b63a
Mason Walker <masonwalker30@gmail.com>
2022-01-15 17:15:39 -0800

    questions added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var questions = [
+    {text:"Who created bitcoin?", choices: ["yes", "no"], correctAnswer: "yes"}, 
+    {text:"?", choices: ["yes","no"]}, 
+    {text:"?", choices: ["yes","no"]},
+    {text:"?", choices: ["yes","no"]},
+    {text:"?", choices: ["yes","no"]},
+    {text:"?", choices: ["yes","no"]},    
+    {text:"?", choices: ["yes","no"]},    
+    {text:"?", choices: ["yes","no"]},    
+    {text:"?", choices: ["yes","no"]},   
+    {text:"?", choices: ["yes","no"]},    
+    {text:"?", choices: ["yes","no"]},
+];
+
+
+// shows the first question, and has buttons to choose an answer
+// when user answers question, show the next question..  (hide the previous question)
 

29079453de40cde8cfc0b9dd78e56f0653fd600e
Mason Walker <masonwalker30@gmail.com>
2022-01-19 16:20:28 -0800

    added all questions for user

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var questions = [
     {text:"Who created bitcoin?", choices: ["yes", "no"], correctAnswer: "yes"}, 
-    {text:"?", choices: ["yes","no"]}, 
-    {text:"?", choices: ["yes","no"]},
-    {text:"?", choices: ["yes","no"]},
-    {text:"?", choices: ["yes","no"]},
-    {text:"?", choices: ["yes","no"]},    
-    {text:"?", choices: ["yes","no"]},    
-    {text:"?", choices: ["yes","no"]},    
-    {text:"?", choices: ["yes","no"]},   
-    {text:"?", choices: ["yes","no"]},    
-    {text:"?", choices: ["yes","no"]},
+    {text:"What 2nd most common coin?", choices: ["Bitcoin","Litecoin","Ethereum","Dogecoin"]}, 
+    {text:"What is Elon Musk's favorite coin?", choices: ["Shiba","Dogecoin","Musky","Litecoin"]},
+    {text:"Who is the famous creator of Bitcoin?", choices: ["Vitalik Buterin","Charles Hoskinson","Satoshi Nakamoto","Jeffrey Bezos"]},
+    {text:"Who is creating the new metaverse META?", choices: ["Jeff Bezos","Mark Zuckerberg","Jack Dorsey","Elon Musk"]},
+    {text:"Which country of our world was the 1st to accept Bitcoin as legal currency?", choices: ["China","Japan","Ukraine","El Salvador"]},    
+    {text:"Who bans Bitcoin the most often?", choices: ["US","United Kingdom","North Korea","China"]},    
+    {text:"Pick one of the 2 easiest platforms for a new crypto investor? (2 of 4 correct answers)", choices: ["Sushi Swap","Coinbase","Crypto.com","Robinhood"]},    
+    {text:"Are you a bitcoin investor?", choices: ["yes","no"]},   
+    {text:"Where is Snoop Dogg building Snoopland?", choices: ["Meta","Decentraland","Sandbox","Alice"]},    
+    {text:"Want to learn more about crypto?", choices: ["yes","no"]},
 ];
 
 

0702c10b6247c42efc992e2190180ec9cf4a35ec
Mason Walker <masonwalker30@gmail.com>
2022-01-24 02:34:12 -0800

    added new object,changed element ID, added appendChild

--- script.js
+++ script.js
@@ -1,43 +1,105 @@
-const startButton = document.getElementById('startButton')
-const time = document.getElementById('time')
-const questionsElement = document.getElementById('questions')
-
+const startButton = document.getElementById("startButton")
+let time = document.getElementById("time")
+let questionsElement = document.getElementById("questions")
 
 let setTime = 60
-let currentQuestion = 0
+let current = 0
 
-var questions = [
-    {text:"Who created bitcoin?", choices: ["yes", "no"], correctAnswer: "yes"}, 
-    {text:"What 2nd most common coin?", choices: ["Bitcoin","Litecoin","Ethereum","Dogecoin"], correctAnswer: "Ethereum"}, 
-    {text:"What is Elon Musk's favorite coin?", choices: ["Shiba","Dogecoin","Musky","Litecoin"], correctAnswer: "Dogecoin"},
-    {text:"Who is the famous creator of Bitcoin?", choices: ["Vitalik Buterin","Charles Hoskinson","Satoshi Nakamoto","Jeffrey Bezos"], correctAnswer: "Satoshi Nakamoto"},
-    {text:"Who is creating the new metaverse META?", choices: ["Jeff Bezos","Mark Zuckerberg","Jack Dorsey","Elon Musk"], correctAnswer: "Jack Dorsey"},
-    {text:"Which country of our world was the 1st to accept Bitcoin as legal currency?", choices: ["China","Japan","Ukraine","El Salvador"], correctAnswer: "El Salvador"},    
-    {text:"Who bans Bitcoin the most often?", choices: ["US","United Kingdom","North Korea","China"], correctAnswer: "China"},    
-    {text:"Pick one of the 2 easiest platforms for a new crypto investor? (2 of 4 correct answers)", choices: ["Sushi Swap","Coinbase","Crypto.com","Robinhood"], correctAnswer: "Coinbase"},    
-    //{text:"Are you a crypto investor?", choices: ["yes","no"], correctAnswer: "yes", "no"},   
-    {text:"Where is Snoop Dogg building Snoopland?", choices: ["Meta","Decentraland","Sandbox","Alice"], correctAnswer: "Sandbox"},    
-   // {text:"Want to learn more about crypto?", choices: ["yes","no"], correctAnswer: "yes", "no"},
-];
+var questions = 
+
+[
+    {question:"What 2nd most common coin?",
+    
+    answers: [
+        
+        { answer: "Bitcoin",},
+
+        {answer: "Litecoin",},
+
+        {answer: "Ethereum", correct: true },
+
+        {answer: "Dogecoin",}
+
+    ],},  
+  
+  {question: "Where is Snoop Dogg building Snoopland?", 
+  
+    answers: [
+      
+        { answer: "Meta"},
+
+        { answer: "Decentraland" },
+
+        { answer: "Sandbox" , correct: true },
+
+        { answer: "Alice" },
+    ],},
+
+    {question: "Where is Snoop Dogg building Snoopland?", 
+  
+    answers: [
+      
+        { answer: "Meta"},
+
+        { answer: "Decentraland" },
+
+        { answer: "Sandbox" , correct: true },
+
+        { answer: "Alice" },
+    ],},
+
+    {question: "Where is Snoop Dogg building Snoopland?", 
+  
+    answers: [
+      
+        { answer: "Meta"},
+
+        { answer: "Decentraland" },
+
+        { answer: "Sandbox" , correct: true },
+
+        { answer: "Alice" },
+    ],}
+
+]
 
 function timer (){
     let alottedTime = setInterval (function(){
         setTime--;
-        time.textContent=setTime+ 'seconds left'
+        time.textContent=setTime+ "seconds left"
     },1000)
 }
 
-startButton.addEventListener('click',timer)
+startButton.addEventListener("click",startQuiz)
 
-function questions(data){
-    let questionBox = document.createElement('h2')
+function cryptoQuestions(data)
+{   let questionBox = document.createElement("div")
+    let questionHeader = document.createElement("h2")
     let currentQuestion = questions[data]
-    questionBox.innerHTML=currentQuestion["text"]
-    questionsElement.append(questionBox)
+    questionHeader.innerHTML = currentQuestion["question"]
+    questionsElement.appendChild(questionHeader)
+    for (let index = 0; index < currentQuestion["answers"].length; index++) 
+    
+    {   const element = currentQuestion[index];
+        let btn = document.createElement("button")
+        btn.innerHTML = currentQuestion["answers"][index]["answer"]
+        answer.value = currentQuestion["answers"][index]["correct"]
+        answer.setAttribute("onClick","answerLog(this.value)")
+        questionsBox.appendChild(answer)
+
+    }   
+        questionsElement.appendChild(questionBox)
 }
 
 
+// have buttons display
+
+
+function startQuiz (){
+    timer()
+    cryptoQuestions(0)
+
+}
 
 // shows the first question, and has buttons to choose an answer
 // when user answers question, show the next question..  (hide the previous question)
-
